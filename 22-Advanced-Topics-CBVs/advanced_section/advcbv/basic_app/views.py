b35cf138ed8337577580b850c17f97efb84245a3
emad_ <emad_566@hotmail.com>
2019-01-17 18:03:24 +0200

    Finsh lesson 158 Hello World with CBVs

--- 22-Advanced-Topics-CBVs/advanced_section/advcbv/basic_app/views.py
+++ 22-Advanced-Topics-CBVs/advanced_section/advcbv/basic_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from django.views.generic import View
+from django.http import HttpResponse
+
+class CBView(View):
+    def get(self, request):
+        return HttpResponse("Class Based View Are Cool!")
+
+
 # Create your views here.
-def index(request):
-    return render(request, "/index.html", context={})
+# def index(request):
+#     return render(request, "/index.html", context={})
