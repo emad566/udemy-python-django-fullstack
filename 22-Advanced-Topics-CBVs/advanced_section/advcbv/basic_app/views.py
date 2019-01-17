28ac0223b4aa7dffc5ec0acee6a2335ed1836bb7
emad_ <emad_566@hotmail.com>
2019-01-17 18:33:19 +0200

    After Finish Lesson 159 Template Views with CBV-subtitle-en

--- 22-Advanced-Topics-CBVs/advanced_section/advcbv/basic_app/views.py
+++ 22-Advanced-Topics-CBVs/advanced_section/advcbv/basic_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
+# from django.http import HttpResponse
 
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Class Based View Are Cool!")
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'Basic Injaction'
+        return context
+
+# class CBView(View):
+#     def get(self, request):
+#         return HttpResponse("Class Based View Are Cool!")
 
 
 # Create your views here.
