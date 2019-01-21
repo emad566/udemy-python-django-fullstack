bce01e332c48950fa635bd017fdd8ffa66d4c85c
emad_ <emad_566@hotmail.com>
2019-01-21 20:49:04 +0200

    After finish minit 13:00 at Lesson 177 Social Clone Part Three

--- 24-Social-Media-Site-Clone-Project/simplesocial/simplesocial/urls.py
+++ 24-Social-Media-Site-Clone-Project/simplesocial/simplesocial/urls.py
@@ -20,6 +20,7 @@ from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.IndexView.as_view(), name='index'),
 ]
