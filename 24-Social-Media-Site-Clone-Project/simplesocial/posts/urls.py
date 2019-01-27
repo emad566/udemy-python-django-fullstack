708aaa30afc5195220b0079029657eac72503e0c
emad_ <emad_566@hotmail.com>
2019-01-27 08:08:29 +0200

    After finshed Lesson 183 Social Clone Part Nine

---
+++ 24-Social-Media-Site-Clone-Project/simplesocial/posts/urls.py
@@ -0,0 +1,27 @@
+"""posts app of simplesocial URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='all'),
+    path('new/', views.CreatePost.as_view(), name='create'),
+    path('by/<int:username>', views.UserPosts.as_view(), name='for_user'),
+    path('by/in/<int:username>/<int:pk>/', views.UserPosts.as_view(), name='single'),
+    path('delete/<int:pk>', views.DeletePost.as_view(), name='delete'),
+]
