93551340c11a7ccdd95dcf534c39cd5aad546427
emad_ <emad_566@hotmail.com>
2019-01-27 03:11:03 +0200

    After finshed Minite 10:00 Lesson 180 Social Clone Part Six

--- 24-Social-Media-Site-Clone-Project/simplesocial/groups/models.py
+++ 24-Social-Media-Site-Clone-Project/simplesocial/groups/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils.text import slugify
-
+from django.urls import reverse
 # Create your models here.
-import misaka
+import misaka 
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -13,14 +13,28 @@ register = template.Library()
 class Group(models.Model):
     name = models.CharField(max_length=264, unique=True)
     slug = models.SlugField(allow_unicode=True, unique=True)
-    
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through="GroupMemper")
+      
     def __str__(self):
-        return self.
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('groups:single', kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering = ['name']
 
 class GroupMemper(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='mempership')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_groups')
-
+    
     def __str__(self):
         return self.user.username
 
